90bbfcf7d364211c36b087e86ac090b45ff1e695
madeldigreg <mdigreg3@uwo.ca>
2022-02-03 14:13:16 -0500

    Menu with level select and algorithm select, redirects on submit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,87 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Formik, Field, Form } from 'formik';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Basic = () => (
+  <div>
+    <h1 h1 style={{backgroundColor: "lightblue", padding: "10px"}}>Sortin'</h1>
+
+    <Formik
+      initialValues={{
+        level: 'one',
+        algorithm: 'mergeSort',
+      }}
+      onSubmit={async (values) => {
+        await new Promise((r) => setTimeout(r, 500));
+        // alert(JSON.stringify(values, null, 2));
+
+        // window.location.href = "http://google.com"
+        // window.location.replace("http://localhost:5500/src/level" + values.level + ".html");
+         window.location.href = "http://localhost:5500/src/level" + values.level + ".html"
+        // return false;
+      }}
+    >
+      <body>
+      <Form>
+        {/* Radio Button Idea */}
+          {/* <div id="my-radio-group">Choose a Level: </div>
+          <div role="group" aria-labelledby="my-radio-group">
+            <label>
+              <Field type="radio" name="level" value="one" checked />
+              One
+            </label>
+            <br />
+            <label>
+              <Field type="radio" name="level" value="two" />
+              Two
+            </label>
+          </div> 
+          
+          <br />
+          <br />
+
+          <div id="my-radio-group2">Choose a Sorting Algorithm: </div>
+          <div role="group">
+            <label>
+              <Field type="radio" name="algorithm" value="mergeSort" checked />
+              Merge Sort
+            </label>
+          </div>
+
+          <br />*/}
+
+          <div id="levelSelect">Choose a Level: </div>
+          <div role="group">
+            <label>
+              <Field as="select" name="level" size="3">
+              <option value="one">Level 1</option>
+              <option value="two">Level 2</option>
+              {/* <option value="three">Level 3</option> */}
+              </Field>
+            </label>
+          </div>
+
+          <br />
+
+          <div id="algorithmSelect">Choose a Sorting Algorithm: </div>
+          <div role="group">
+            <label>
+              <Field as="select" name="algorithm" size="3">
+              <option value="mergeSort">Merge Sort</option>
+              </Field>
+            </label>
+          </div>
+          
+          <br />
+
+          <button type="submit">Submit</button>
+        </Form>
+        </body>
+    </Formik>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+<Basic />, 
+document.getElementById('root')
+);
