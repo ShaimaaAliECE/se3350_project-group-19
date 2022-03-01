8809608d131eed91669cdc84aa2f330207a85ba2
bwolf02 <bwolf2@uwo.ca>
2022-03-01 16:29:58 -0500

    changed homepage

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import generateMergeSteps from "./sort";
 import generateRandomArray from "./rand_array";
 import Level1 from "./Level1.js";
-import { Routes, Route, Link } from "react-router-dom";
+import {Routes, Route, Link } from "react-router-dom";
 import Menu from "./Menu.js"
 import Level2 from "./Level2.js";
 import Level3 from "./Level3";
